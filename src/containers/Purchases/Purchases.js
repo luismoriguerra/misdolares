25e23b3dab4040c16fe915f6006f9e25b817304e
isaac mori <luis.mori@globant.com>
2018-07-26 20:36:15 -0500

    footer btonts

--- src/containers/Purchases/Purchases.js
+++ src/containers/Purchases/Purchases.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import PurchaseForm  from './PurchaseForm';
 import PurchaseList from './PurchaseList/PurchaseList';
 
 export default class Purchases extends Component {
 
   render() {
     return (
-      <div>
-        <PurchaseForm />
-        <PurchaseList />
-      </div>
+      <PurchaseList />
     )
   }
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, withRouter, Link } from 'react-router-dom'
 import Purchases from './Purchases/Purchases';
 import { History } from './Purchases/History/History';
 import styled from 'styled-components';
-import { Breadcrumb } from 'antd';
+import PurchaseForm from './PurchaseForm/PurchaseForm';
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,28 +11,44 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
+const Content = styled.div`
+    flex: 1;
+`;
+
+const Footer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    `;
+const Button = styled.div`
+    border: 1px solid #ccc;
+    width: 100%;
+    flex: 1;
+    padding: 20px;
+    text-align: center;
+`;
 
 const Root = (props) => {
 
-  return (
-      <Wrapper>
-        <Router>
-        <React.Fragment>
-            <Breadcrumb>
-                <Breadcrumb.Item key="home">
-                <Link to="/">Home</Link>
-                </Breadcrumb.Item>
-            </Breadcrumb>
-            <Switch>
-                <Route path="/:id" component={History} />
-                <Route path="/" component={Purchases} />
-            </Switch>
-        </React.Fragment>
-
-          
-        </Router>
-      </Wrapper>
-  )
+    return (
+        <Wrapper className="root-wrapper">
+            <Router>
+                <React.Fragment>
+                    <Content className="content">
+                        <Switch className="switch">
+                        <Route exact path="/" component={Purchases} />
+                        <Route exact path="/form" component={PurchaseForm} />
+                            <Route path="/history/:id" component={History} />
+                        </Switch>
+                    </Content>
+                    <Footer className="footer">
+                        <Button><Link to="/">Compras</Link></Button>
+                        <Button><Link to="/form">Agregar</Link></Button>
+                        <Button><Link to="/chart">Grafico</Link></Button>
+                    </Footer>
+                </React.Fragment>
+            </Router>
+        </Wrapper>
+    )
 };
 
 export default Root;
